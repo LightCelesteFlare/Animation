b6028cc014974579b7788aa72e384c366b2d5a7e
Brandon <Brandonkool16@gmail.com>
2018-01-07 22:25:59 -0500

    [Add]

---
+++ Animation/Assets/Scripts/Condiction.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Condiction : MonoBehaviour {
+    public Animator anim;
+    public int action;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (action == 1)
+        { 
+        action = 1;
+		anim.SetInteger("inputAct", action);
+        }
+        if (action == 2)
+        {
+            action = 2;
+            anim.SetInteger("inputAct", action);
+        }
+        if (action == 3)
+        {
+            action = 3;
+            anim.SetInteger("inputAct", action);
+        }
+
+    }
+}
